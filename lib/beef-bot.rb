c25b4c5ea658a85eaac4df21c4344d847dd1f8df
elebertus <bobcrotch@gmail.com>
2011-06-20 18:42:27 -0400

    The bot will now op on join, and added !opme

--- lib/beef-bot.rb
+++ lib/beef-bot.rb
@@ -1,52 +1,46 @@
+# todo: add adn ADMINS plugin to list, and modify
+# the list of admins, or people who can delete
+# users in the AOP list
+#
+# Figure out the :static and :admins
+
 require 'redis'
 module Cinch::Plugins
   module BEEF
+
+
+
     class REDIS 
 
-      # Since this is mostly a learning exercise
-      # I wrote a class to interact with Redis
-      # This could all be removed and have the 
-      # Cinch::Plugin classes connect to Redis
-      # directly
       attr_writer :chan
 
-      # Channel name without hash
-      def chan
-        @chan = "channel"
-      end
-
-      # This is assuming you're running redis locally
-      # and have it configured to write the .sock file
-      # to /tmp
-      def initialize(*args)
-        super(*args)
-        @db = Redis.new(:path => "/tmp/redis.sock")
-      end
-      
-      # Print all users in the aop list
-      def print_list
-        @db.smembers(chan)
-      end
-  
-      # Check to see if the users exists
-      def user_check(name)
-        @db.sismember(chan, name)
-      end
-
-      # Add a user to the aop list
-      # since this is a list the check
-      # could be removed
-      def user_add(name)
-        v = @db.sismember(chan, name).to_s
-        if v == "false"
-         @db.sadd(chan, name)
+        def chan
+          @chan = "dongtown"
         end
-      end
-   
-      # Delete a user from the aop list 
-      def user_del(name)
-        @db.srem(chan, name)
-      end
+
+        def initialize(*args)
+          super(*args)
+          @db = Redis.new(:path => "/tmp/redis.sock")
+        end
+
+        def print_list
+          @db.smembers(@chan)
+        end
+
+        def user_check(name)
+          @db.sismember(@chan, name)
+        end
+
+        def user_add(name)
+          v = @db.sismember(@chan, name).to_s
+          if v == "false"
+           @db.sadd(@chan, name)
+          end
+        end
+       
+        def user_del(name)
+          @db.srem(@chan, name)
+        end
     end
 
     class SEARCH
@@ -58,10 +52,9 @@ module Cinch::Plugins
       end
 
       match %r/(search\s.+)/, :use_prefix => true, :use_suffix => true
+ 
       react_on :channel
 
-      # This might be a hack to clean up the query
-      # so that it's the bare username
       def execute(m, query)
         response = query.gsub(/search/, "").lstrip
         unless !@db.user_check(response)
@@ -82,6 +75,7 @@ module Cinch::Plugins
       end
 
       match %r/(add\s.+)/, :use_prefix => true, :use_suffix => true
+      
       react_on :channel
 
       def execute(m, query)
@@ -97,17 +91,12 @@ module Cinch::Plugins
       attr_writer :static
       attr_writer :admins
 
-      # A list of users who cannot be removed from the
-      # aop list.
       def static
-        @static = %w[foo bar baz]
+        @static = %w[eblack erikh PopeKetric Dav3xor sam]
       end
 
-      # A list of admins users who can delete users
-      # from the aop list. This could also be used
-      # if you wanted to restric the other actions.
       def admins
-        @admins = %w[foo]
+        @admins = %w[eblack]
       end
 
       def initialize(*args)
@@ -118,9 +107,6 @@ module Cinch::Plugins
       match %r/(delete\s.+)/, :use_prefix => true, :use_suffix => true
       react_on :channel
 
-      # Checks to see if the calling user is in the
-      # @admins list. If they are, it verifies that
-      # the user being deleted is not in @static
       def execute(m, query)
         user = query.gsub(/delete/, "").lstrip
         op = "#{m.user.nick}"
@@ -142,7 +128,6 @@ module Cinch::Plugins
       end
 
       match %r/(help)/, :use_prefix => true, :use_suffix => false
-      react_on :channel
 
       def execute(m)
         m.user.send "Welcome to #dongtown!"
@@ -150,6 +135,42 @@ module Cinch::Plugins
         m.user.send "To see if a user is on the aop list use !search nick"
         m.user.send "To add a user to the list use !add user"
       end
+    end
+
+    class OPONJOIN
+      include Cinch::Plugin
+    
+      def initialize(*args)
+        super(*args)
+        @db = REDIS.new
+      end
+
+      listen_to :join
+
+      def listen(m)
+        unless m.user.nick == bot.nick
+          if @db.user_check("#{m.user.nick}")
+            m.channel.op(m.user)
+          end
+        end
+      end
+    end
+
+    class OPME
+      include Cinch::Plugin
+      def initialize(*args)
+       super(*args)
+       @db = REDIS.new
+      end
+
+     match %r/(opme)/, :use_prefix => true, :use_suffix => false
+     react_on :channel
+
+     def execute(m)
+       if @db.user_check("#{m.user.nick}")
+         m.channel.op(m.user)
+       end
+     end
     end
 
     class PRINT
